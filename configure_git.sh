f62196ce3828dc8e50bf27c934d566f95805e9e5
Thanasis Karetsos <karetsosjr@gmail.com>
2011-12-11 22:14:54 +0200

    add git configuration script

---
+++ configure_git.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# ~/dotfiles/configure_git.sh
+#
+# configure git color and aliases
+#
+
+# set color
+`git config --global color.ui auto`
+
+# set aliases
+`git config --global alias.co checkout`
+`git config --global alias.br branch`
+`git config --global alias.cv "commit -v"`
+`git config --global alias.st status`
+`git config --global alias.ff diff`
+`git config --global alias.cav "commit -v -a"`
+`git config --global alias.cam "commit -a -m"`
