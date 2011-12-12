bca6b9065e349573159c14f242042ebe38e79171
Thanasis Karetsos <karetsosjr@gmail.com>
2011-12-12 17:35:13 +0200

    remove back ticks

--- configure_git.sh
+++ configure_git.sh
@@ -6,13 +6,13 @@
 #
 
 # set color
-`git config --global color.ui auto`
+git config --global color.ui auto
 
 # set aliases
-`git config --global alias.co checkout`
-`git config --global alias.br branch`
-`git config --global alias.cv "commit -v"`
-`git config --global alias.st status`
-`git config --global alias.ff diff`
-`git config --global alias.cav "commit -v -a"`
-`git config --global alias.cam "commit -a -m"`
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.cv "commit -v"
+git config --global alias.st status
+git config --global alias.ff diff
+git config --global alias.cav "commit -v -a"
+git config --global alias.cam "commit -a -m"
