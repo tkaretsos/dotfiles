0da997c004a38e752184d25215af7d325393b7c6
Thanasis Karetsos <karetsosjr@gmail.com>
2012-11-04 14:25:15 +0100

    use modulo for toggling instead of if

--- util_scripts/toggle_touchpad.sh
+++ util_scripts/toggle_touchpad.sh
@@ -7,9 +7,5 @@
 touchpad_id=$(xinput | grep -i touchpad | grep -o "id=[0-9]*" | sed 's/[^0-9]//g')
 is_enabled=$(xinput --list-props $touchpad_id | grep -i enabled | grep -o "[0-9]$")
 
-if [[ $is_enabled == "0" ]]; then
-  xinput --set-prop $touchpad_id "Device Enabled" 1
-else
-  xinput --set-prop $touchpad_id "Device Enabled" 0
-fi
+xinput --set-prop $touchpad_id "Device Enabled" $(expr $(expr $is_enabled + 1) % 2)
 
