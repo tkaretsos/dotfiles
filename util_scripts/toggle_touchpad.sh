94784d6165ec0959c936dff408f4b93f6447e07a
Thanasis Karetsos <karetsosjr@gmail.com>
2012-12-23 03:38:19 +0100

    add arguments to toggle_touchpad

--- util_scripts/toggle_touchpad.sh
+++ util_scripts/toggle_touchpad.sh
@@ -7,5 +7,11 @@
 touchpad_id=$(xinput | grep -i touchpad | grep -o "id=[0-9]*" | sed 's/[^0-9]//g')
 is_enabled=$(xinput --list-props $touchpad_id | grep -i enabled | grep -o "[0-9]$")
 
-xinput --set-prop $touchpad_id "Device Enabled" $(expr $(expr $is_enabled + 1) % 2)
+if [[ $# -eq 0 ]]; then
+  xinput --set-prop $touchpad_id "Device Enabled" $(expr $(expr $is_enabled + 1) % 2)
+elif [[ $1 == "on" ]]; then
+  xinput --set-prop $touchpad_id "Device Enabled" 1
+elif [[ $1 == "off" ]]; then
+  xinput --set-prop $touchpad_id "Device Enabled" 0
+fi
 
