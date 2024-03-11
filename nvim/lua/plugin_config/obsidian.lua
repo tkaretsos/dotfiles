159fc9acef51c5b518612b31dc42bd25e8ca0d29
Thanasis Karetsos <tkaretsos@gmail.com>
2024-03-11 13:58:05 +0200

    Update obsidian config

--- nvim/lua/plugin_config/obsidian.lua
+++ nvim/lua/plugin_config/obsidian.lua
@@ -1,12 +1,32 @@
-require("obsidian").setup({
+local obsidian = require("obsidian")
+
+obsidian.setup({
   workspaces = {
-    {
-      name = "Personal",
-      path = "~/obsidian/Personal",
-    },
     {
       name = "Work",
       path = "~/obsidian/Work",
     },
+    {
+      name = "Personal",
+      path = "~/obsidian/Personal",
+    },
+  },
+
+  daily_notes = {
+    folder = "dailies",
+    -- Optional, if you want to change the date format for the ID of daily notes.
+    date_format = "%Y-%m-%d",
+    -- Optional, if you want to change the date format of the default alias of daily notes.
+    alias_format = "%B %-d, %Y",
+    -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+    template = "daily.md"
+  },
+
+  templates = {
+    subdir = "templates",
+    date_format = "%Y-%m-%d",
+    time_format = "%H:%M",
+    -- A map for custom variables, the key should be the variable and the value a function
+    substitutions = {},
   },
 })
