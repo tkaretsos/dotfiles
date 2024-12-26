ce0a7d556e1d06005a2cfd2bc24386bc7744a7d9
Thanasis Karetsos <thanasis@karetsos.me>
2024-12-26 11:42:08 +0200

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
+# Linux Mint Wilma (22)
+
 set -e
 cd $HOME
 mkdir -p $HOME/Code
+DOTFILES=$HOME/Code/dotfiles
 
 # Install utilities
 echo "Installing apt packages..."
-sudo apt-get -y -qq install build-essential git curl zsh vim jq ripgrep
-
-echo
-
-DOTFILES=$HOME/Code/dotfiles
+sudo apt-get -y -qq install build-essential git curl zsh vim jq ripgrep fzf fd-find neovim
 
 # Clone dotfiles
 if ! [ -d $DOTFILES ]; then
@@ -26,8 +25,8 @@ fi
 # Install asdf
 if ! [ -d $HOME/.asdf ]; then
   echo "Installing asdf..."
-  ASDF_LATEST=$(curl -s "https://api.github.com/repos/asdf-vm/asdf/tags" | jq -r '.[0].name')
-  git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch $ASDF_LATEST
+  ASDF_VERSION=$(curl -s "https://api.github.com/repos/asdf-vm/asdf/tags" | jq -r '.[0].name')
+  git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch $ASDF_VERSION
   if [ -f $DOTFILES/.tool-versions ]; then
     cp $DOTFILES/.tool-versions $HOME
   fi
@@ -45,7 +44,27 @@ else
   echo "neovim already installed"
 fi
 
-echo
+# Install Nerd font
+if ! [ -d $HOME/.local/share/fonts/DejaVuSansMNerdFont ]; then
+  echo "Installing nerd font..."
+  mkdir -p $HOME/.local/share/fonts
+  wget -P /tmp https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/DejaVuSansMono.zip
+  unzip /tmp/DejaVuSansMono.zip -d $HOME/.local/share/fonts/DejaVuSansMNerdFont
+  fc-cache -fv
+else
+  echo "nerd font already installed"
+fi
+
+# Install Lazygit
+if ! [ -x "$(command -v lazygit)" ]; then
+  echo "Installing lazygit..."
+  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | jq -r '.tag_name')
+curl -Lo /tmp/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+  tar xf /tmp/lazygit.tar.gz /tmp/lazygit
+  sudo install /tmp/lazygit -D -t /usr/local/bin/
+else
+  echo "lazygit already installed"
+fi
 
 # Install ulauncher
 if ! [ -x "$(command -v ulauncher)" ]; then
@@ -58,8 +77,6 @@ if ! [ -x "$(command -v ulauncher)" ]; then
 else
   echo "ulauncher already installed"
 fi
-
-echo
 
 # Install oh-my-zsh
 if ! [ -d $HOME/.oh-my-zsh ]; then
@@ -79,3 +96,5 @@ if ! [ -d $HOME/.oh-my-zsh ]; then
 else
   echo "oh-my-zsh already installed"
 fi
+
+echo "Setup finished. Reboot is required."
