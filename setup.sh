8b4351bfe2eafb775898a0cbd1fe1e7959306504
Thanasis Karetsos <thanasis@karetsos.me>
2024-12-26 08:38:26 +0200

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,7 @@
 
 set -e
 cd $HOME
-CODE_DIR=$HOME/Code
-mkdir -p $CODE_DIR
+mkdir -p $HOME/Code
 
 # Install utilities
 echo "Installing apt packages..."
@@ -11,15 +10,17 @@ sudo apt-get -y -qq install build-essential git curl zsh vim jq ripgrep
 
 echo
 
+DOTFILES=$HOME/Code/dotfiles
+
 # Clone dotfiles
-if ! [ -d $CODE_DIR/dotfiles ]; then
+if ! [ -d $DOTFILES ]; then
   echo "Cloning dotfiles..."
-  git clone git@github.com:tkaretsos/dotfiles $CODE_DIR/dotfiles
+  git clone git@github.com:tkaretsos/dotfiles $DOTFILES
   echo
   echo "Setting up configuration files..."
-  ln -s $CODE_DIR/dotfiles/.vimrc $HOME/.vimrc
-  ln -s $CODE_DIR/dotfiles/.vim $HOME/.vim
-  cp $CODE_DIR/dotfiles/.gitconfig $HOME
+  ln -s $DOTFILES/.vimrc $HOME/.vimrc
+  ln -s $DOTFILES/.vim $HOME/.vim
+  cp $DOTFILES/.gitconfig $HOME
 fi
 
 # Install asdf
@@ -27,8 +28,21 @@ if ! [ -d $HOME/.asdf ]; then
   echo "Installing asdf..."
   ASDF_LATEST=$(curl -s "https://api.github.com/repos/asdf-vm/asdf/tags" | jq -r '.[0].name')
   git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch $ASDF_LATEST
+  if [ -f $DOTFILES/.tool-versions ]; then
+    cp $DOTFILES/.tool-versions $HOME
+  fi
 else
   echo "asdf already installed"
+fi
+
+# Install neovim
+if ! [ -x "$(command -v nvim)" ]; then
+  echo "Installing neovim..."
+  asdf plugin add neovim
+  asdf install neovim stable
+  asdf global neovim stable
+else
+  echo "neovim already installed"
 fi
 
 echo
@@ -40,7 +54,7 @@ if ! [ -x "$(command -v ulauncher)" ]; then
   sudo apt update
   sudo apt-get -y install ulauncher
   rm $HOME/.config/ulauncher/settings.json
-  ln -s $CODE_DIR/dotfiles/.config/ulauncher/settings.json $HOME/.config/ulauncher/settings.json
+  ln -s $DOTFILES/.config/ulauncher/settings.json $HOME/.config/ulauncher/settings.json
 else
   echo "ulauncher already installed"
 fi
@@ -59,7 +73,7 @@ if ! [ -d $HOME/.oh-my-zsh ]; then
   git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
   rm $HOME/.zshrc
-  ln -s $CODE_DIR/dotfiles/.zshrc $HOME/.zshrc
+  ln -s $DOTFILES/.zshrc $HOME/.zshrc
 
   echo "Log out and log in required"
 else
