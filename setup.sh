6a1dc15ad159c68f82dd6ebd22318c63b3c874df
Thanasis Karetsos <thanasis@karetsos.me>
2024-12-27 19:43:25 +0200

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,19 +3,17 @@
 # Linux Mint Wilma (22)
 
 set -e
-cd $HOME
 mkdir -p $HOME/Code
 DOTFILES=$HOME/Code/dotfiles
 
 # Install utilities
 echo "Installing apt packages..."
-sudo apt-get -y -qq install build-essential git curl zsh vim jq ripgrep fzf fd-find neovim
+sudo apt-get -y install build-essential git curl zsh vim jq ripgrep fzf fd-find neovim
 
 # Clone dotfiles
 if ! [ -d $DOTFILES ]; then
   echo "Cloning dotfiles..."
   git clone git@github.com:tkaretsos/dotfiles $DOTFILES
-  echo
   echo "Setting up configuration files..."
   ln -s $DOTFILES/.vimrc $HOME/.vimrc
   ln -s $DOTFILES/.vim $HOME/.vim
@@ -44,6 +42,51 @@ else
   echo "NodeJS already installed"
 fi
 
+# Install go
+if ! [ -x "$(command -v go)" ]; then
+  echo "Installing golang..."
+  asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
+  asdf install golang latest
+  asdf global golang latest
+else
+  echo "golang already installed"
+fi
+
+# Install erlang asdf plugin
+if ! asdf plugin list | grep erlang &>/dev/null; then
+  echo "Installing erlang asdf plugin..."
+  # required packages for erlang
+  sudo apt install -y autoconf libssl-dev libncurses-dev
+
+  # rebar is required by Mason NeoVim plugin to install erlang-ls
+  if ! [ -x "$(command -v rebar3)"]; then
+    echo "Installing rebar3..."
+    asdf plugin add rebar https://github.com/Stratus3D/asdf-rebar.git
+    asdf install rebar latest
+    asdf global rebar latest
+  fi
+
+  asdf plugin add erlang https://github.com/asdf-vm/asdf-erlang.git
+  echo "Install the desired erlang/OTP version:"
+  echo "$ asdf list all erlang"
+  echo "$ asdf install erlang <version>"
+  echo "$ asdf global erlang <version>"
+else
+  echo "erlang asdf plugin already installed"
+fi
+
+# Install elixir asdf plugin
+if ! asdf plugin list | grep elixir &>/dev/null; then
+  echo "Installing elixir asdf plugin..."
+  asdf plugin add elixir https://github.com/asdf-vm/asdf-elixir.git
+  echo "Install the desired elixir version:"
+  echo "$ asdf list all elixir"
+  echo "$ asdf install elixir <version>"
+  echo "$ asdf global elixir <version>"
+else
+  echo "elixir asdf plugin already installed"
+fi
+
 # Install Nerd font
 if ! [ -d $HOME/.local/share/fonts/DejaVuSansMNerdFont ]; then
   echo "Installing nerd font..."
@@ -58,9 +101,10 @@ fi
 # Install Lazygit
 if ! [ -x "$(command -v lazygit)" ]; then
   echo "Installing lazygit..."
-  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | jq -r '.tag_name')
-curl -Lo /tmp/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-  tar xf /tmp/lazygit.tar.gz /tmp/lazygit
+  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | jq -r '.tag_name' | cut -c2-)
+  echo "Lazygit version = $LAZYGIT_VERSION"
+  curl -Lo /tmp/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+  tar xf /tmp/lazygit.tar.gz -C /tmp
   sudo install /tmp/lazygit -D -t /usr/local/bin/
 else
   echo "lazygit already installed"
@@ -97,4 +141,4 @@ else
   echo "oh-my-zsh already installed"
 fi
 
-echo "Setup finished. Reboot is required."
+echo "Setup finished."
